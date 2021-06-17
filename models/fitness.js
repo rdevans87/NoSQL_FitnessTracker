d933e13bc42f5af090b55e71103212863db64226
Ryan Evans <rdevans87@gmail.com>
2021-06-17 13:47:03 -0400

    formatting fitness data into db data

--- models/fitness.js
+++ models/fitness.js
@@ -4,6 +4,7 @@
 // Add new exercises to a new workout plan.
 // View the combined weight of multiple exercises from the past seven workouts on the `stats` page.
 // View the total duration of each workout from the past seven  workouts on the `stats` page.
+// The user can log multiple exercises in a workout on a given day as well as track the name, type, weight, sets, reps, and duration of exercise. 
 
 
 const mongoose = require("mongoose");
@@ -13,19 +14,32 @@ const Schema = mongoose.Schema;
 const fitnessSchema = new Schema({
     date: {
         type: Date,
-        default: Date.now
+        default: Date.now,
       },
     exercises: [
     {
-        type: string,
+     type: {
+        type: String,
         trim: true,
-        required: "Add exercises to workout plan."
-      },
-      value: {
+        required: "Excercise Type."
+    },
+     name: {
+       type: String,
+       trim: true,
+       required: "Exercise Name."
+    },
+     duration: {
         type: Number,
-        required: "Enter an amount"
-      },
+
+
+    }
      
+   
+   
+   
+   
+   
+   
     });
     
 
