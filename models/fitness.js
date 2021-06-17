b2e0eafb00d9c8184953000ad996ff16a00117db
Ryan Evans <rdevans87@gmail.com>
2021-06-17 15:00:31 -0400

    schema completed moving back to models

--- models/fitness.js
+++ models/fitness.js
@@ -15,57 +15,47 @@ const fitnessSchema = new Schema({
     date: {
         type: Date,
         default: Date.now,
-      },
+    },
     exercises: [
-    {
-     type: {
-        type: String,
-        trim: true,
-        required: "Excercise Type."
-    },
-     name: {
-       type: String,
-       trim: true,
-       required: "Exercise Name."
-    },
-     duration: {
-        type: Number,
-        required: "Excercise Duration."
-    },
-     distance: {
-        type: Number,
-    },
-    weight: {
-       type: Number,
-    
-    },
-    reps: {
-       type: Number,
-    },
-    sets: {
-        type: Number,
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Excercise Type."
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Exercise Name."
+            },
+            duration: {
+                type: Number,
+                required: "Excercise Duration."
+            },
+            distance: {
+                type: Number,
+            },
+            weight: {
+                type: Number,
 
-    },
-  },
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
 
-],
-    
+            },
+        },
+    ],
+
 });
 
-const Fitness = 
-   
-   
-   
-   
-   
-   
-    });
-    
+const Fitness = mongoose.model("Fitness", fitnessSchema);
+
+module.exports = Fitness;
 
 
-]
-    const Transaction = mongoose.model("Transaction", transactionSchema);
-    
-    module.exports = Transaction;
-    
-}
+
+
+
