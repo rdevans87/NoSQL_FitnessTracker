6f1ff73195fd268965e0bc34e8405d370923f25f
Ryan Evans <rdevans87@gmail.com>
2021-06-17 08:43:03 -0400

    updated serverjs to reflect workout tracker routes and local host

--- server.js
+++ server.js
@@ -9,3 +9,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+// may need to update file to workout.js instead of fitness.js to avoid confusion.
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
