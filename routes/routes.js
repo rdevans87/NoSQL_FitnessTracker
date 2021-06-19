04f3e6263c3e8bf581d5a045de268b7a557051e9
Ryan Evans <rdevans87@gmail.com>
2021-06-18 22:17:27 -0400

    routes complete

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,20 @@
 
 
 const router = require("express").Router();
-const path =  require("path");module
+const path =  require("path");
+
+router.get('/', (req,res) => {
+    res.sendFile(path.join(_dirmane, "../public/index.html"));
+
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(_dirnname, "../public/stats.html"));
+
+});
+
+
+module.exports = router;
+
+
+
